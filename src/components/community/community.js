855256c86dce66b0f9d2cdf9c6d8f60dff05c62e
Haley Moses <vhr825@gmail.com>
2022-06-21 02:37:59 -0500

    added capybara video

--- src/components/community/community.js
+++ src/components/community/community.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react";
 import './community.scss';
-
+import capyvideo from '../../assets/Capybaras pull up.mp4';
 
 const Community = () => {
+    
+    const videoEl = useRef(null);
 
+    const attemptPlay = () => {
+      videoEl &&
+        videoEl.current &&
+        videoEl.current.play().catch(error => {
+          console.error("Error attempting to play", error);
+        });
+    };
+  
+    useEffect(() => {
+      attemptPlay();
+    }, []);
     return (
+
         <div className='communityoutercontainer'>
+
             <div className='communitycontainter'>
+          
                 <div className='communitycontainercolor'>
+                
                     <div className='communitycontainercoloraccentline'>
-                        <div className='communitycontainercapybara'>
+                        <video
+                            className="capyvideoclass"
+                            style={{ maxWidth: "100%" }}
+                            playsInline
+                            loop
+                            muted
+                            controls
+                            alt="All the devices"
+                            src={capyvideo}
+                            ref={videoEl}
+                        />
+                        <div className="communitycontainercapybara">
                             Capybara Community
                         </div>
                     </div>
